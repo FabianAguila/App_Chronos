9c44c84788788c0b56f2073b8150e7082ffc5b9a
BrayanPe <brayanpenaloza57@gmail.com>
2025-11-29 00:00:04 -0300

    arreglo error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,7 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    
+  firebaseConfig: {
+
   },
-<<<<<<< HEAD
-  apiUrl:"https://apichronos-production.up.railway.app/api"
-=======
-  apiUrl:"https://dev.matiivilla.cl/duoc/location/"
->>>>>>> 4045baf (Frontend v1.1)
+  apiUrl: "https://apichronos-production.up.railway.app/api"
 };
